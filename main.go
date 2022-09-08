85c646a9160c2ec9304a1538172c20d73002fc74
Ramish Hassan <ramishshah93@gmail.com>
2022-09-08 21:22:21 +0200

    built logic for checking for MX and SPF records

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"net"
 	"os"
+	"strings"
 )
 
 func main(){
@@ -23,4 +25,28 @@ func main(){
 
 func checkDomain(domain string){
 	
+	var hasMX, hasSPF, hasDMAC bool
+	var spfRecord, dmarcRecord string
+
+	mxRecords, err :=net.LookupMX(domain)
+
+	if err != nil {
+		log.Printf("Error %v", err)
+	}
+	if len(mxRecords) > 0 {
+		hasMX = true
+	}
+
+	txtRecords, err:=net.LookupTXT(domain)
+	if err != nil {
+		log.Printf("Error %v", err)
+	}
+
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf1"){
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+	}
 }
