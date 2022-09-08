2e41453f4b032aa2748f9eeaa5af22448d8530d1
Ramish Hassan <ramishshah93@gmail.com>
2022-09-08 20:58:19 +0200

    scaning input with bufio package.

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-func main(){
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
+func main(){
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("domain, hasMX, hasSPF, sprRecord, HasDMARC, dmarcRecord \n")
+
+	for scanner.Scan(){
+		checkDomain(scanner.Text())
+
+	}
+
+	if err := scanner.Err(); err != nil{
+		log.Fatal(err, "could not read from input")
+	}
 }
 
-func checkDomain(){
+func checkDomain(domain string){
 	
 }
